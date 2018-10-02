1b7768bb7e0d4318a6f71365b6018c7e4ddf90aa
Felipe Dutra <f3liperamos@gmail.com>
2018-10-01 21:55:29 -0300

    Converting into pure html and js

--- src/app.js
+++ src/app.js
@@ -1,49 +1,9 @@
-const { store, actions } = require('./store-redux')
-const createElement = require('./create-element')
+const { actions, store } = require('./redux/redux-store')
 
-const Container = createElement({ element: 'div', className: 'container' })
-const H1 = createElement({ element: 'h1', className: 'title' })
-H1.textContent = generateTitle()
+const updateMirrorInput = require('./mirror-inputs')
+const updateLights = require('./lights')
+const watcher = require('./watcher')
 
-const Input1 = createElement({
-  element: 'input',
-  attributes: { autofocus: true, type: 'text' },
-  className: 'input',
-  eventListeners: {
-    input(event) {
-      store.dispatch({
-        type: actions.INPUT_ACTION,
-        value: event.target.value
-      })
-    }
-  }
-})
-
-const Input2 = createElement({
-  element: 'input',
-  attributes: { type: 'text' },
-  className: 'input2'
-})
-
-console.log(Input1)
-
-function updateView() {
-  const inputValue = store.getState().value
-
-  Input2.value = inputValue.split('').reverse().join('')
-  H1.textContent = generateTitle(inputValue)
-}
-
-store.subscribe(updateView)
-
-function generateTitle(value) {
-  return !value ? 'YARL.js' : `Hello, ${value}`
-}
-
-Container.appendChild(H1)
-Container.appendChild(Input1)
-Container.appendChild(Input2)
-
-
-
-window.document.body.appendChild(Container)
+store.subscribe(updateMirrorInput)
+store.subscribe(updateLights)
+store.subscribe(watcher)
